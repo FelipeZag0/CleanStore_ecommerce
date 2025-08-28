1c4fa6fde6a52c5f5bf4ffee7454bb5aa97e5b7b
FelipeZag0 <felipezago29@gmail.com>
2025-08-28 12:42:36 -0300

    feat(controller): adicionar endpoint GET /pedidos para listar todos os pedidos

--- src/main/java/com/ecommerce/pedidos_api/controller/PedidoController.java
+++ src/main/java/com/ecommerce/pedidos_api/controller/PedidoController.java
@@ -7,15 +7,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/pedidos")
 @RequiredArgsConstructor
 public class PedidoController {
+
     private final PedidoService pedidoService;
 
+    // Cria um novo pedido
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PedidoResponseDTO criarNovoPedido(@RequestBody PedidoRequestDTO pedidoRequestDTO) {
         return pedidoService.criarPedido(pedidoRequestDTO);
+    }
+
+    // Lista todos os pedidos cadastrados
+    @GetMapping
+    public List<PedidoResponseDTO> listarTodosOsPedidos() {
+        return pedidoService.listarTodos();
+    }
+
+    // Busca um pedido específico pelo seu ID
+    @GetMapping("/{id}")
+    public PedidoResponseDTO obterPedidoPorId(@PathVariable Long id) {
+        return pedidoService.buscarPorId(id);
     }
 }
